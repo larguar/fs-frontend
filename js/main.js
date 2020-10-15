43d8d9210cc96432afab81b69eab8b67bfa924b1
siminski <laurensiminski@gmail.com>
2020-10-15 19:03:22 -0400

    done for now

--- js/main.js
+++ js/main.js
@@ -77,30 +77,61 @@ menuClose.on('click', () => {
 –––––––––––––––––––––––––––––– */
 
 const saveModal = $('.save-modal');
-const saveClose = $('.save-modal .close');
-const saveButton = $('.card-image');
 
-saveButton.on('click', () => {
+$('.card-image').on('click', () => {
 	saveModal.attr('style', 'opacity: 1');
 	saveModal.show();
 	return false;
 });
 
-saveClose.on('click', () => {
+$('.save-modal .close').on('click', () => {
 	saveModal.attr('style', 'opacity: 0');
 	saveModal.hide();
 	return false;
 });
 
+$('#saveToBoard').on('submit', event => {
+	event.preventDefault();
+	
+	const alert = $('#saveToBoard .alert');
+	const alertText = $('#saveToBoard .alert-copy');
+	
+	const board = $('#board').val();
+	
+	console.log('Board:', board);
+	// Code to add image to board through image id
+	
+	alert.show();
+	alertText.html('<strong>Success!</strong> This post has been saved to ' + board + '.');
+	setTimeout(() => {
+		alert.hide();
+		saveModal.attr('style', 'opacity: 0');
+		saveModal.hide();
+		return false;
+	}, 2000);
+});
 
 
 
+/* SORT BY
+–––––––––––––––––––––––––––––– */
+
+const sort = $('#sort');
+
+sort.on('change', () => {
+	if (sort.val() === 'Newest First') {
+		console.log('Sort By:', sort.val());
+		// Code to sort by date	
+	} else if (sort.val() === 'Closest First') {
+		console.log('Sort By:', sort.val());		
+		// Code to sort by proximity/location		
+	}
+});
 
 
 
-
-
-
+/* SEARCH RESULTS
+–––––––––––––––––––––––––––––– */
 
 const search = $('#search');
 const searchResults = $('.search-results');
@@ -118,10 +149,8 @@ search.on('keyup', () => {
 
 
 
-
-
-
-
+/* IMAGE DROP
+–––––––––––––––––––––––––––––– */
 
 function readDisplayImage(input) {
   if (input.files && input.files[0]) {
@@ -140,7 +169,7 @@ function readReplaceImage(input) {
   if (input.files && input.files[0]) {
     const reader = new FileReader();
     reader.onload = function(e) {
-      $('.file-upload-content').attr('style', 'background-image: url(' + e.target.result + '); background-size: cover; background-position: center center;');
+      $('.file-upload-content').show().attr('src', e.target.result);
     };
     reader.readAsDataURL(input.files[0]);
     $('.image-upload-wrap').hide();
@@ -158,4 +187,41 @@ function removeToPlaceholder() {
 function removeAndRestart() {
 	$('.image-upload-wrap').show();
 	$('.file-upload-content').hide();
+}
+
+
+
+/* ADD POST
+–––––––––––––––––––––––––––––– */
+
+$('#add-post').on('submit', event => {
+	event.preventDefault();
+	console.log('Image:', $('#add-post .file-upload-content').attr('src'));
+	console.log('Location:', $('#add-post #location').val());
+	console.log('Caption:', $('#add-post #caption').val());
+	console.log('Hashtags:', $('#add-post #hashtags').val());
+});
+
+
+
+/* SHORT DESCRIPTION
+–––––––––––––––––––––––––––––– */
+
+const pullCaption = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+
+const cardText = $('.card-text');
+const longText = pullCaption.split(' ');
+const shortText = longText.slice(0, 10);
+
+const less = '<span class="text">' + shortText.join(' ') + '</span><span class="more" onclick="showMore()"> … more</span>';
+const more = '<span class="text">' + longText.join(' ') + '</span><span class="less" onclick="showLess()"> … less</span>';
+
+cardText.html(less);
+
+function showMore() {
+	cardText.html(more);
+}
+
+function showLess() {
+	cardText.html(less);
 }
